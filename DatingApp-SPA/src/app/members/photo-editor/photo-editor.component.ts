2d447955f8d6c9670e07d5f0477dba94f9b68950
Celmad <manuelalaminosd@protonmail.com>
2019-07-17 18:01:32 +0200

    Added third party photo uploader

---
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { Photo } from 'src/app/_models/photo';
+import { environment } from 'src/environments/environment';
+import { AuthService } from 'src/app/_services/auth.service';
+
+@Component({
+  selector: 'app-photo-editor',
+  templateUrl: './photo-editor.component.html',
+  styleUrls: ['./photo-editor.component.css']
+})
+export class PhotoEditorComponent implements OnInit {
+  @Input() photos: Photo[];
+  uploader: FileUploader;
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.initializeUploader();
+  }
+
+  fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/' + this.authService.decodedToken.nameId + '/photos',
+      authToken: 'Bearer' + localStorage.getItem('token'),
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
+  }
+}
