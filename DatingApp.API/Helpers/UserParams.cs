9eced31e95dc879e00196ac9c10a39f95d27e346
Celmad <manuelalaminosd@protonmail.com>
2019-09-04 00:18:52 +0200

    GetUsers method in repository now can retrieve Likees and Likers users

--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -16,5 +16,7 @@ namespace DatingApp.API.Helpers
         public int MinAge { get; set; } = 18;
         public int MaxAge { get; set; } = 130;
         public string OrderBy { get; set; }
+        public bool Likees { get; set; } = false;
+        public bool Likers { get; set; } = false;
     }
 }
