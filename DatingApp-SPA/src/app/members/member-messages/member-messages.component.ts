668f82ecc67aed7a9cefa38f27dccf4dde30c32e
Celmad <manuelalaminosd@protonmail.com>
2020-01-08 22:59:26 +0100

    Read messages are marked now as such

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -3,6 +3,7 @@ import { Message } from 'src/app/_models/message';
 import { UserService } from 'src/app/_services/user.service';
 import { AuthService } from 'src/app/_services/auth.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-member-messages',
@@ -24,12 +25,22 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages() {
+    const currentUserId = +this.authService.decodedToken.nameid; // + convert to a number, to compare it to another number below
     this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
-    .subscribe(messages => {
-      this.messages = messages;
-    }, error => {
-      this.alertify.error(error);
-    });
+      .pipe(
+        tap(messages => {
+          for (let i = 0; i < messages.length; i++) {
+            if (messages[i].isRead === false && messages[i].recipientId === currentUserId) {
+              this.userService.markAsRead(currentUserId, messages[i].id);
+            }
+          }
+        })
+      )
+      .subscribe(messages => {
+        this.messages = messages;
+      }, error => {
+        this.alertify.error(error);
+      });
   }
 
   sendMessage() {
