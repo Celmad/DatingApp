e845d9c44eb9054650357da2c8b7d0ba3dd68d12
Celmad <manuelalaminosd@protonmail.com>
2020-01-21 14:38:20 +0100

    Added back exception handler. Deleted developer exception page in production

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -114,21 +114,20 @@ namespace DatingApp.API
             }
             else
             {
-                // app.UseExceptionHandler(builder => {
-                //     builder.Run(async context => {
-                //         context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                app.UseExceptionHandler(builder => {
+                    builder.Run(async context => {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-                //         var error = context.Features.Get<IExceptionHandlerFeature>();
-                //         if (error != null)
-                //         {
-                //             context.Response.AddApplicationError(error.Error.Message);
-                //             await context.Response.WriteAsync(error.Error.Message);
-                //         }
-                //     });
-                // });
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if (error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
                 app.UseHsts();
             }
-            app.UseDeveloperExceptionPage();
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseAuthentication();
